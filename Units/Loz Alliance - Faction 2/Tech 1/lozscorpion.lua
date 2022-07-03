498eadd0f85d7fe29e84e5c1fe82f835a5a000ae
ForbodingAngel <forbodingangel@gmail.com>
2022-07-02 22:02:19 -0700

    Add loz scorpion

---
+++ Units/Loz Alliance - Faction 2/Tech 1/lozscorpion.lua
@@ -0,0 +1,21 @@
+--------------------------------------------------------------------------------
+
+unitName = "lozscorpion"
+
+--------------------------------------------------------------------------------
+
+humanName = [[Scorpion]]
+
+objectName = "lozscorpion.s3o"
+script = "lozscorpion.cob"
+
+tech = [[tech1]]
+
+VFS.Include("units-configs-basedefs/basedefs/Loz Alliance - Faction 2/Tier 1/lozscorpion_basedef.lua")
+
+unitDef.weaponDefs = weaponDefs
+--------------------------------------------------------------------------------
+
+return lowerkeys({ [unitName]    = unitDef })
+
+--------------------------------------------------------------------------------
