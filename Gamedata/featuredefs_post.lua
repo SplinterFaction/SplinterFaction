fd84757731e9b167defed1192cd4216c9800caa1
ForbodingAngel <forbodingangel@gmail.com>
2022-09-25 22:24:16 -0700

    Fix reclaimtimes on stupid maps? Second Try

--- Gamedata/featuredefs_post.lua
+++ Gamedata/featuredefs_post.lua
@@ -50,14 +50,25 @@ for name, fd in pairs(FeatureDefs) do
 	end
 
 	if tonumber(fd["metal"]) == nil then
-		fd.metal = 0
+		fd["metal"]) = 1
 	end
 
 	if tonumber(fd["energy"]) == nil then
-		fd.energy = 0
+		fd["energy"]) = 1
 	end
-	
-	fd.reclaimtime = fd.metal * 0.25
+
+	if tonumber(fd["metal"]) >= tonumber(fd["energy"]) then
+		fd["reclaimtime"] = fd["metal"] * 0.25
+		-- Spring.Echo("Based off of metal")
+		-- Spring.Echo(fd.name)
+		-- Spring.Echo(fd.reclaimtime)
+
+	else
+		fd["reclaimtime"] = fd["energy"] * 0.25
+		-- Spring.Echo("Based off of energy")
+		-- Spring.Echo(fd.name)
+		-- Spring.Echo(fd.reclaimtime)
+	end
 	
 	
 	-- Reset maximum feature values
