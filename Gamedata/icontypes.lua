ec9017f00d2fedccc0ae5813f6b29070cb025ed7
ForbodingAngel <forbodingangel@gmail.com>
2022-09-27 01:29:57 -0700

    Add new unit icons

--- Gamedata/icontypes.lua
+++ Gamedata/icontypes.lua
@@ -9,559 +9,95 @@
 --------------------------------------------------------------------------------
 
 local icontypes = {
-  default = {
-	bitmap='icons/default.png',
+	default = {
+		bitmap='icons/nato/default.dds',
 		size=1,
-		radiusadjust=3,
+		radiusadjust=true,
 	},
 
-  -- modifications by dmdr start here
-  
-		
+	commander = {
+		bitmap='icons/nato/commander.dds',
+		size=1,
+		radiusadjust=true,
+	},
+
 	engineer = {
-		bitmap='icons/dmdr_engi.png',
-		size=3,
-		radiusadjust=0,
-	},
-  
-		orb = {
-		bitmap='icons/dmdr_orb.png',
-		size=3,
-		radiusadjust=0,
+		bitmap='icons/nato/engineer.dds',
+		size=1,
+		radiusadjust=true,
 	},
 
-	box = {
-		bitmap='icons/dmdr_box.png',
-		size=2,
-		radiusadjust=0,
-	},
-	
-	boxsmall = {
-		bitmap='icons/dmdr_box.png',
+	economy = {
+		bitmap='icons/nato/economy.dds',
 		size=1,
-		radiusadjust=0,
+		radiusadjust=true,
 	},
-  
-	raider = {
-		bitmap='icons/dmdr_raider.png',
-		size=1.5,
-		radiusadjust=0,
+
+	supportbuilding = {
+		bitmap='icons/nato/supportbuilding.dds',
+		size=1,
+		radiusadjust=true,
 	},
-  
-	krow = {
-		bitmap='icons/dmdr_end_krow.png',
-		size=3,
-		radiusadjust=0,
+
+	defenseturret = {
+		bitmap='icons/nato/defense-turret.dds',
+		size=1,
+		radiusadjust=true,
 	},
-  
-	leveller = {
-		bitmap='icons/dmdr_end_lvlr.png',
-		size=3,
-		radiusadjust=0,
+
+	shield = {
+		bitmap='icons/nato/shield.dds',
+		size=1,
+		radiusadjust=true,
 	},
-  
-	lacerator = {
-		bitmap='icons/dmdr_end_lacr.png',
-		size=3,
-		radiusadjust=0,
+
+	factory = {
+		bitmap='icons/nato/factory.dds',
+		size=1,
+		radiusadjust=true,
 	},
-  
-	anarchid = {
-		bitmap='icons/dmdr_end_nrkd2.png',
-		size=3,
-		radiusadjust=0,
+
+	antiair = {
+		bitmap='icons/nato/anti-air.dds',
+		size=1,
+		radiusadjust=true,
 	},
-  
-	shield_lit = {
-		bitmap='icons/dmdr_shield_mobile.png',
-		size=2.5,
-		radiusadjust=0,
-	},
-  
-  
-	riot_lit = {
-		bitmap='icons/dmdr_riot_lit.png',
-		size=2.2,
-		radiusadjust=0,
-	},
-  
-	riot_arm = {
-		bitmap='icons/dmdr_riot_arm.png',
-		size=2.2,
-		radiusadjust=0,
-	},
-  
-	arty_lit = {
-		bitmap='icons/dmdr_arty_lit.png',
-		size=2.2,
-		radiusadjust=0,
-	},
-  
-	arty_arm = {
-		bitmap='icons/dmdr_arty_arm.png',
-		size=2.2,
-		radiusadjust=0,
-	},
-  
-	td_arm_all = {
-		bitmap='icons/dmdr_td_ab.png',
-		size=2.2,
-		radiusadjust=0,
-	},
-  
-	td_arm_lit = {
-		bitmap='icons/dmdr_td_al.png',
-		size=2.2,
-		radiusadjust=0,
-	},
-  
-	td_arm_arm = {
-		bitmap='icons/dmdr_td_aa.png',
-		size=2.2,
-		radiusadjust=0,
-	},
-		
-	td_lit_all = {
-		bitmap='icons/dmdr_td_lb.png',
-		size=2.2,
-		radiusadjust=0,
-	},
-  
-	td_lit_lit = {
-		bitmap='icons/dmdr_td_ll.png',
-		size=2.2,
-		radiusadjust=0,
-	},
-  
-	td_lit_arm = {
-		bitmap='icons/dmdr_td_la.png',
-		size=2.2,
-		radiusadjust=0,
-	},
-  
-		  mtd_lit_arm = {
-		bitmap='icons/dmdr_mtd_la.png',
-		size=2.2,
-		radiusadjust=0,
-	},
-  
-	mtd_lit_all = {
-		bitmap='icons/dmdr_mtd_lb.png',
-		size=2.2,
-		radiusadjust=0,
-	},
-  
-		  mtd_arm_arm = {
-		bitmap='icons/dmdr_mtd_aa.png',
-		size=2.2,
-		radiusadjust=0,
-	},
-  
+
 	scout = {
-		bitmap='icons/dmdr_air_sct.png',
-		size=1.5,
-		radiusadjust=0,
-	},
-  
-	etransport = {
-		bitmap='icons/dmdr_tport.png',
-		size=2.5,
-		radiusadjust=0,
-	},
-  
-	air_lit = {
-		bitmap='icons/dmdr_air_lit.png',
-		size=2.2,
-		radiusadjust=0,
-	},
-  
-	air_arm = {
-		bitmap='icons/dmdr_air_arm.png',
-		size=2.2,
-		radiusadjust=0,
-	},
-  
-	air_bomb = {
-		bitmap='icons/dmdr_air_bmb.png',
-		size=2.2,
-		radiusadjust=0,
-	},
-  
-	drone = {
-		bitmap='icons/dmdr_air_all.png',
+		bitmap='icons/nato/scout.dds',
 		size=1,
-		radiusadjust=0,
-	},
-  
-	turret_lit = {
-		bitmap='icons/defense.png',
-		size=2.2,
-		radiusadjust=0,
-	},
-  
-	turret_arm = {
-		bitmap='icons/defense.png',
-		size=3,
-		radiusadjust=0,
-	},
-  
-	turret_arty = {
-		bitmap='icons/trt_art.png',
-		size=3,
-		radiusadjust=0,
+		radiusadjust=true,
 	},
 
-	esilo = {
-		bitmap='icons/dmdr_nuke.png',
-		size=4,
-		radiusadjust=0,
+	scoutvision = {
+		bitmap='icons/nato/scout-vision.dds',
+		size=1,
+		radiusadjust=true,
 	},
 
-	radar = {
-		bitmap='icons/radar.png',
+	mbt = {
+		bitmap='icons/nato/mbt.dds',
 		size=1,
-		radiusadjust=0,
-	},
-  
-	radarjam = {
-		bitmap='icons/radarjam.png',
-		size=1,
-		radiusadjust=0,
-	},
-  
-	storage = {
-		bitmap='icons/support.png',
-		size=1,
-		radiusadjust=3,
-	},
-  
-	energy = {
-		bitmap='icons/dmdr_tech.png',
-		size=3,
-		radiusadjust=0,
-	},
-  
-	generator = {
-		bitmap='icons/dmdr_energy.png',
-		size=2,
-		radiusadjust=3,
-	},
-  
-	baseshield = {
-		bitmap='icons/dmdr_buckler.png',
-		size=3.5,
-		radiusadjust=0,
-	},
-  
-	nukeshield = {
-		bitmap='icons/dmdr_shield_nuke.png',
-		size=3.5,
-		radiusadjust=0,
-	},
-  
-  -- and end here
-
-  eco = {
-		bitmap='icons/eco.png',
-		size=1,
-		radiusadjust=3,
-	},  
-
-  air = {
-		bitmap='icons/air.png',
-		size=1,
-		radiusadjust=3,
-	},
-  factory = {
-		bitmap='icons/factory.png',
-		size=1,
-		radiusadjust=3,
+		radiusadjust=true,
 	},
 
-  barricade = {
-		bitmap='icons/barricade.png',
+	firesupport = {
+		bitmap='icons/nato/fire-support.dds',
 		size=1,
-		radiusadjust=3,
-	},
-  
-  support = {
-		bitmap='icons/support.png',
-		size=1,
-		radiusadjust=3,
-	},
-  
-  commander = {
-		bitmap='icons/commander.png',
-		size=1,
-		radiusadjust=3,
+		radiusadjust=true,
 	},
 
-
-  ORB = {
-		bitmap='icons/orb.png',
+	artillery = {
+		bitmap='icons/nato/artillery.dds',
 		size=1,
-		radiusadjust=3,
-	},
-
- --------Zaals--------- 
- 
- -- land
-  zlightswarm = {
-	bitmap='icons/z/zswarm.png',
-	size=2.5,
-	radiusadjust=0
-	},
-  
-  zmedswarm = {
-	bitmap='icons/z/zmedswarm.png',
-	size=3,
-	radiusadjust=0
-	},
-  
-  zskirmisher = {
-	bitmap='icons/z/zskirmisher.png',
-	size=3,
-	radiusadjust=0
-	},
-  
-  zassault = {
-	bitmap='icons/z/zassault.png',
-	size=3.5,
-	radiusadjust=0
-	},
-  
-  zpyro = {
-	bitmap='icons/z/zpyro.png',
-	size=3,
-	radiusadjust=0
-	},
-  
-  zarty = {
-	bitmap='icons/z/zarty.png',
-	size=3.5,
-	radiusadjust=0
-	},
-  
-  zqueen = {
-	bitmap='icons/z/zqueen.png',
-	size=8,
-	radiusadjust=0
-	},
-  
-  -- air
-  zairscout = {
-	bitmap='icons/z/zairscout.png',
-	size=2,
-	radiusadjust=0
-	},
-  
-  zairtoairfighter = {
-	bitmap='icons/z/zairtoairfighter.png',
-	size=3.5,
-	radiusadjust=0
-	},
-  
-  zairtogroundfighter = {
-	bitmap='icons/z/zairtogroundfighter.png',
-	size=3.5,
-	radiusadjust=0
-	},
-  
-   zbomber = {
-	bitmap='icons/z/zbomber.png',
-	size=4,
-	radiusadjust=0
-	},
-  -- buildings
-  
-  
- ----------------------
-		
-  chicken = 
-	{
-		bitmap="icons/chicken.png",
-		size=1.4,
-		},
-	chickena = 
-	{
-		bitmap="icons/chickena.png",
-		size=1.6,
-		},
-	chickenf = {
-		bitmap="icons/chickenf.png",
-		size=1.6,
-		},
-	chickens = 
-	{
-		bitmap="icons/chickens.png",
-		size=1.5,
-		},
-	chickenr = {
-		bitmap="icons/chickenr.png",
-		size=1.6,
-		},
-	chickenq = {
-		bitmap="icons/chickenq.png",
-		size=3,
-		},
-	chikenqueen = {
-		bitmap='icons/dmdr_end_nrkd2.png',
-		size=8,
-	},
-	chickencommander = {
-		bitmap='icons/commander.png',
-		size=3,
-	},
-  
-  --[[ all this stuff is redundant now, so for legibility I'm commenting it out (dmdr 271216)
-  
-		tankdestroyer = {
-		bitmap='icons/tankdestroyer.png',
-		size=1,
-		radiusadjust=3,
-	},
-  
-  assault = {
-		bitmap='icons/assault.png',
-		size=1,
-		radiusadjust=3,
-	},
-  
-  aa = {
-		bitmap='icons/aa.png',
-		size=1,
-		radiusadjust=3,
-	},
-  
-  
-		etransport = {
-		bitmap='icons/support.png',
-		size=2.2,
-		radiusadjust=3,
-	},
-  
-  esilo = {
-		bitmap='icons/esilo.png',
-		size=1,
-		radiusadjust=3,
+		radiusadjust=true,
 	},
 
-  elrpc = {
-		bitmap='icons/elrpc.png',
+	assault = {
+		bitmap='icons/nato/assault.dds',
 		size=1,
-		radiusadjust=3,
+		radiusadjust=true,
 	},
-
-  hover = {
-		bitmap='icons/hover.png',
-		size=1,
-		radiusadjust=3,
-	},
-  
-  hoverraider = {
-		bitmap='icons/hoverraider.png',
-		size=1,
-		radiusadjust=3,
-	},
-  
-  hovertankdestroyer = {
-		bitmap='icons/hovertankdestroyer.png',
-		size=1,
-		radiusadjust=3,
-	},
-  
-  hoverassault = {
-		bitmap='icons/hoverassault.png',
-		size=1,
-		radiusadjust=3,
-	},
-  
-  hoveraa = {
-		bitmap='icons/hoveraa.png',
-		size=1,
-		radiusadjust=3,
-	},
-
-  amphib = {
-		bitmap='icons/amphib.png',
-		size=1,
-		radiusadjust=3,
-	},
-  
-  amphibraider = {
-		bitmap='icons/amphibraider.png',
-		size=1,
-		radiusadjust=3,
-	},
-  
-  amphibtankdestroyer = {
-		bitmap='icons/amphibtankdestroyer.png',
-		size=1,
-		radiusadjust=3,
-	},
-  
-  amphibassault = {
-		bitmap='icons/amphibassault.png',
-		size=1,
-		radiusadjust=3,
-	},
-  
-  amphibaa = {
-		bitmap='icons/amphibaa.png',
-		size=1,
-		radiusadjust=3,
-	},
-
-  allterr = {
-		bitmap='icons/allterr.png',
-		size=1,
-		radiusadjust=3,
-	},
-  
-  allterrraider = {
-		bitmap='icons/raider.png',
-		size=1,
-		radiusadjust=3,
-	},
-  
-  allterrtankdestroyer = {
-		bitmap='icons/allterrtankdestroyer.png',
-		size=1,
-		radiusadjust=3,
-	},
-  
-  allterrassault = {
-		bitmap='icons/allterrassault.png',
-		size=1,
-		radiusadjust=3,
-	},
-  
-  allterraa = {
-		bitmap='icons/allterraa.png',
-		size=1,
-		radiusadjust=3,
-	},
-
-  sub = {
-		bitmap='icons/sub.png',
-		size=1,
-		radiusadjust=3,
-	}, 
-  
-		defense = {
-		bitmap='icons/defense.png',
-		size=1,
-		radiusadjust=3,
-	},
-  
-		storage = {
-		bitmap='icons/storage.png',
-		size=1,
-		radiusadjust=3,
-	},
-  
-  ]]--
-  
 }
 
 --------------------------------------------------------------------------------
