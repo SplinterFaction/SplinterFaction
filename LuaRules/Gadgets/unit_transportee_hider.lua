bfae7b5c8ededcd47d276b7de2187aee5ccd0bf9
ForbodingAngel <forbodingangel@gmail.com>
2022-10-25 00:24:53 -0700

    make it pretty

--- LuaRules/Gadgets/unit_transportee_hider.lua
+++ LuaRules/Gadgets/unit_transportee_hider.lua
@@ -1,13 +1,13 @@
 function gadget:GetInfo()
-  return {
-    name      = "Transportee Hider",
-    desc      = "Hides units when inside a closed transport, issues stop command to units trying to enter a full transport",
-    author    = "FLOZi",
-    date      = "09/02/10",
-    license   = "PD",
-    layer     = 0,
-    enabled   = false  --  loaded by default?
-  }
+    return {
+        name      = "Transportee Hider",
+        desc      = "Hides units when inside a closed transport, issues stop command to units trying to enter a full transport",
+        author    = "FLOZi",
+        date      = "09/02/10",
+        license   = "PD",
+        layer     = 0,
+        enabled   = false  --  loaded by default?
+    }
 end
 
 local SetUnitNoDraw            = Spring.SetUnitNoDraw
