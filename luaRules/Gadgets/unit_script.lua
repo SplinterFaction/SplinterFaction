fba5c111d3fb0e89dbf80e500e5ccff737bbad2d
ForbodingAngel <forbodingangel@gmail.com>
2022-11-09 15:16:18 -0800

    Revert "change case on base folder names"
    This reverts commit dc4afae9ab1f48dd02c50cb892815b7069ddffd1.

--- luaRules/Gadgets/unit_script.lua
+++
@@ -1,6 +0,0 @@
--- Enables Lua unit scripts by including the gadget from springcontent.sdz
-
--- Uncomment to override the directory which is scanned for *.lua unit scripts.
---UNITSCRIPT_DIR = "scripts/"
-
-return VFS.Include("LuaGadgets/Gadgets/unit_script.lua", nil, VFS.ZIP_ONLY)
