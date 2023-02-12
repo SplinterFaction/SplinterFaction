dd8595aff75869bd405ee5adbf7372b62f18f2c7
ForbodingAngel <forbodingangel@gmail.com>
2023-02-12 15:52:09 -0800

    0.32

--- modinfo.lua
+++ modinfo.lua
@@ -14,7 +14,7 @@ return {
 	description='SplinterFaction',
 	shortname='SF',
 	version='$VERSION',
-	--version='$VERSION',
+	version='0.32',
 	mutator='Official',
 	game='SplinterFaction',
 	shortGame='SF',
