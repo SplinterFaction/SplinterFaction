e7574ebbaffd9ea5bb9d8a276d16f3783c760364
ForbodingAngel <forbodingangel@gmail.com>
2023-02-19 18:15:59 -0800

    0.42 - Swarm

--- modinfo.lua
+++ modinfo.lua
@@ -14,7 +14,7 @@ return {
 	description='SplinterFaction',
 	shortname='SF',
 	version='$VERSION',
-	version='0.41 - Swarm',
+	version='0.42 - Swarm',
 	mutator='Official',
 	game='SplinterFaction',
 	shortGame='SF',
