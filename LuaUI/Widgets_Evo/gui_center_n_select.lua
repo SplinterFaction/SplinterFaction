44042f47b71965548fb6039b504f4f9b9f79d731
ForbodingAngel <forbodingangel@gmail.com>
2023-03-23 00:10:59 -0700

    Fix some hotkeys Remove old mex spot effects Add new mex spot effect Fix select and center camera on game start

--- LuaUI/Widgets_Evo/gui_center_n_select.lua
+++ LuaUI/Widgets_Evo/gui_center_n_select.lua
@@ -39,14 +39,40 @@ function widget:Update()
     unitArray = Spring.GetTeamUnits(Spring.GetMyTeamID())
     if (go and unitArray[1]) then
       local x, y, z = Spring.GetUnitPosition(unitArray[1])
-      Spring.SetCameraTarget(x, y, z)
-      Spring.SelectUnitArray{unitArray[1]}
-      go = false
+      -- Spring.SetCameraTarget(x, y, z)
+      Spring.SetCameraState({
+                              name=spring,
+                              mode=2,
+                              dist=900,
+                              px=x,
+                              py=y,
+                              pz=z,
+                            }, 5) -- It's not documented, but the 5 here is transition time/speed. Internally I think it's referred to as "camTime".
+    --  These are the arguments for Spring.SetCameraState() for the spring camera
+    --  name, spring
+    --  dist, 3150.0271
+    --  px, 3171.03955
+    --  py, 211.709076
+    --  pz, 882.631409
+    --  rz, 0
+    --  dx, 0
+    --  dy, -0.8940042
+    --  dz, -0.4480586
+    --  fov, 45
+    --  ry, 0
+    --  mode, 2
+    --  rx, 2.67700005
+    --  Spring.SelectUnitArray{unitArray[1]}
+
+    go = false
     end
     if (not go) then
       widgetHandler:RemoveWidget()
     end
   end
+  --for index,value in pairs(Spring.GetCameraState()) do
+  --  Spring.Echo(index,value)
+  --end
 end
 
 
--- modinfo.lua
+++ modinfo.lua
@@ -14,7 +14,7 @@ return {
 	description='SplinterFaction',
 	shortname='SF',
 	version='$VERSION',
-	version='0.51',
+	-- version='0.51',
 	mutator='Official',
 	game='SplinterFaction',
 	shortGame='SF',
