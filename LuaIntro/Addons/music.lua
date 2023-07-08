d9af861843a7a31f6aed1c29d5b03ddc7d6cf5db
Damgam <c_2_damiano@op.pl>
2023-07-08 20:56:32 +0200

    Updated loading screen music widget to the one from BAR

--- LuaIntro/Addons/music.lua
+++ LuaIntro/Addons/music.lua
@@ -3,45 +3,50 @@ if addon.InGetInfo then
 	return {
 		name    = "Music",
 		desc    = "plays music",
-		author  = "jK",
-		date    = "2012,2013",
+		author  = "jK, Damgam",
+		date    = "2012,2013, 2023",
 		license = "GPL2",
 		layer   = 0,
-		depend  = {"LoadProgress"},
 		enabled = true,
 	}
 end
 
-------------------------------------------
-math.randomseed( os.time() - os.clock() * 1000 )
-math.random() math.random() math.random()
-local music_volume_set = Spring.GetConfigInt("snd_volmusic", 20) * 0.01 or 0.2
-Spring.SetSoundStreamVolume(music_volume_set)
-
-musicfiles = VFS.DirList("music/original/loading", "*.ogg")
-tracks = musicfiles
-newTrack = tracks[math.random(1, #tracks)]
-
-if newTrack == nil then
-	Spring.Echo("[LuaIntro Music Player] newTrack is returning nil for some reason")
-	Spring.Echo("[LuaIntro Music Player] number of tracks is " .. #tracks)
-	newTrack = "LuaUI/widgets/Music/loading/Cyberpunk - Crash.ogg"
-	Spring.Echo("[LuaIntro Music Player] Now Playing " .. newTrack)
-	Spring.PlaySoundStream(newTrack)
-	Spring.SetSoundStreamVolume(music_volume_set)
-else
-	Spring.Echo("[LuaIntro Music Player] number of tracks is " .. #tracks)
-	Spring.Echo("[LuaIntro Music Player] Now Playing " .. newTrack)
-	Spring.PlaySoundStream(newTrack)
-	Spring.SetSoundStreamVolume(music_volume_set)
-end
-
---function addon.DrawLoadScreen()
-	--Spring.SetSoundStreamVolume(100)
---end
+function addon.Initialize()
+	if Spring.GetConfigInt('music_loadscreen', 1) == 1 then
+		local originalSoundtrackEnabled = Spring.GetConfigInt('UseSoundtrackNew', 1)
+		local customSoundtrackEnabled	= Spring.GetConfigInt('UseSoundtrackCustom', 1)
+		local allowedExtensions = "{*.ogg,*.mp3}"
 
 
-function addon.Shutdown()
-	--Spring.StopSoundStream()
-	--Spring.SetSoundStreamVolume(0.5)
+		local musicPlaylist = {}
+		if originalSoundtrackEnabled == 1 then
+			local musicDirOriginal 		= 'music/original'
+			table.append(musicPlaylist, VFS.DirList(musicDirOriginal..'/loading', allowedExtensions))
+		end
+
+		-- Custom Soundtrack List
+		if customSoundtrackEnabled == 1 then
+			local musicDirCustom 		= 'music/custom'
+			table.append(musicPlaylist, VFS.DirList(musicDirCustom..'/loading', allowedExtensions))
+		end
+
+		if #musicPlaylist == 0 or math.random(0,3) == 0 then
+			if originalSoundtrackEnabled == 1 then
+				local musicDirOriginal 		= 'music/original'
+				table.append(musicPlaylist, VFS.DirList(musicDirOriginal..'/peace', allowedExtensions))
+			end
+		end
+
+		local musicvolume = Spring.GetConfigInt("snd_volmusic", 20) * 0.01 or 0.2
+		if #musicPlaylist > 1 then
+			local pickedTrack = math.ceil(#musicPlaylist*math.random())
+			Spring.PlaySoundStream(musicPlaylist[pickedTrack], 1)
+			Spring.SetSoundStreamVolume(musicvolume)
+			Spring.SetConfigString('music_loadscreen_track', musicPlaylist[pickedTrack])
+		elseif #musicPlaylist == 1 then
+			Spring.PlaySoundStream(musicPlaylist[1], 1)
+			Spring.SetSoundStreamVolume(musicvolume)
+			Spring.SetConfigString('music_loadscreen_track', musicPlaylist[1])
+		end
+	end
 end
