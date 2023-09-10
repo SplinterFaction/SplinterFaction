ff997c99f3706825e44779484c6f2ddddf1873ec
ForbodingAngel <forbodingangel@gmail.com>
2023-09-10 00:05:36 -0700

    Add start of new unit radar icons

--- Gamedata/icontypes.lua
+++ Gamedata/icontypes.lua
@@ -9,6 +9,131 @@
 --------------------------------------------------------------------------------
 
 local icontypes = {
+	botengineer = {
+		bitmap='icons/supcomish/botengineer.png',
+		size=2,
+		--radiusadjust=true,
+	},
+	botraidert1 = {
+		bitmap='icons/supcomish/botraidert1.png',
+		size=2,
+		--radiusadjust=true,
+	},
+	botraidert3 = {
+		bitmap='icons/supcomish/botraidert3.png',
+		size=2,
+		--radiusadjust=true,
+	},
+	botassaultt1 = {
+		bitmap='icons/supcomish/botassaultt1.png',
+		size=2,
+		--radiusadjust=true,
+	},
+	botassaultt2 = {
+		bitmap='icons/supcomish/botassaultt2.png',
+		size=2,
+		--radiusadjust=true,
+	},
+	botassaultt3 = {
+		bitmap='icons/supcomish/botassaultt3.png',
+		size=2,
+		--radiusadjust=true,
+	},
+	botassaultt4 = {
+		bitmap='icons/supcomish/botassaultt4.png',
+		size=2,
+		--radiusadjust=true,
+	},
+	botfiresupportt2 = {
+		bitmap='icons/supcomish/botfiresupportt2.png',
+		size=2,
+		--radiusadjust=true,
+	},
+	botartilleryt1 = {
+		bitmap='icons/supcomish/botartilleryt1.png',
+		size=2,
+		--radiusadjust=true,
+	},
+	botartilleryt2 = {
+		bitmap='icons/supcomish/botartilleryt2.png',
+		size=2,
+		--radiusadjust=true,
+	},
+	botaat1 = {
+		bitmap='icons/supcomish/botaat1.png',
+		size=2,
+		--radiusadjust=true,
+	},
+	botaat2 = {
+		bitmap='icons/supcomish/botaat2.png',
+		size=2,
+		--radiusadjust=true,
+	},
+	botcounterintelt3 = {
+		bitmap='icons/supcomish/botcounterintelt3.png',
+		size=2,
+		--radiusadjust=true,
+	},
+
+
+	airintelt1 = {
+		bitmap='icons/supcomish/airintelt1.png',
+		size=2,
+		--radiusadjust=true,
+	},
+	airaat1 = {
+		bitmap='icons/supcomish/airaat1.png',
+		size=2,
+		--radiusadjust=true,
+	},
+	airassaultt2 = {
+		bitmap='icons/supcomish/airassaultt2.png',
+		size=2,
+		--radiusadjust=true,
+	},
+	airbombert1 = {
+		bitmap='icons/supcomish/airbombert1.png',
+		size=2,
+		--radiusadjust=true,
+	},
+	airbombernuket3 = {
+		bitmap='icons/supcomish/airbombernuket3.png',
+		size=2,
+		--radiusadjust=true,
+	},
+	airtransportt1 = {
+		bitmap='icons/supcomish/airtransportt1.png',
+		size=2,
+		--radiusadjust=true,
+	},
+	airtransportt2 = {
+		bitmap='icons/supcomish/airtransportt2.png',
+		size=2,
+		--radiusadjust=true,
+	},
+	airtransportt3 = {
+		bitmap='icons/supcomish/airtransportt3.png',
+		size=2,
+		--radiusadjust=true,
+	},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	----------------------------------
+	----------------------------------
 	default = {
 		bitmap='icons/nato/default.dds',
 		size=1,
