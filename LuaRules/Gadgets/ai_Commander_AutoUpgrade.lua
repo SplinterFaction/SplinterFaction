fedf8da36be718c5144b74f1b70826c397849f67
ForbodingAngel <forbodingangel@gmail.com>
2023-10-30 12:46:38 -0700

    AI commanders now upgrade properly

--- LuaRules/Gadgets/ai_Commander_AutoUpgrade.lua
+++ LuaRules/Gadgets/ai_Commander_AutoUpgrade.lua
@@ -2,9 +2,9 @@ function gadget:GetInfo()
 	return {
 		name      = "AI Commander AutoUpgrade",
 		desc      = "Automatically add Commanders to the upgrade queue",
-		author    = "",
-		date      = "",
-		license   = "",
+		author    = "Your Name",
+		date      = "Date",
+		license   = "GPL",
 		layer     = 0,
 		enabled   = true
 	}
@@ -33,14 +33,6 @@ for unitDefID, unitDef in pairs(UnitDefs) do
 	end
 end
 
-function gadget:UnitCreated(unitID, unitDefID, teamID)
-	if UnitDefs[unitDefID].customParams.unitrole == "Commander" then
-		if not (UnitDefs[unitDefID].customParams.requiretech == "tech4") then -- Don't add T4
-			commanders[unitID] = true
-		end
-	end
-end
-
 -- Get my current tech level the ghetto way
 function gadget:UnitFinished(unitID, unitDefID, unitTeam)
 	if UnitDefs[unitDefID].customParams.unitrole == "Commander" then
@@ -63,41 +55,52 @@ function gadget:UnitFinished(unitID, unitDefID, unitTeam)
 	end
 end
 
+function gadget:UnitCreated(unitID, unitDefID, teamID)
+	local unitDef = UnitDefs[unitDefID]
+	if unitDef.customParams.unitrole == "Commander" then
+		if unitDef.customParams.requiretech ~= "tech4" then
+			commanders[unitID] = teamID
+		end
+	end
+end
+
 function gadget:UnitDestroyed(unitID)
 	commanders[unitID] = nil
 end
 
 function gadget:GameFrame(frame)
-	--Let the player know that they have enough income to tech up
-	if frame > 450 then -- 450 = 15 seconds
-		if frame%450 == 18 then
-			--Spring.Echo("My tech level is: " .. myTechLevel)
-			for unitID in pairs(commanders) do
+	if frame > 450 then
+		if frame % 450 == 18 then
+			for unitID, teamID in pairs(commanders) do
 				local unitTeam = Spring.GetUnitTeam(unitID)
-				if not (commTechLevel[Spring.GetUnitDefID(unitID)] >= teamTechLevel[unitTeam]) and Spring.GetTeamLuaAI(unitTeam) then -- We need to use this to check and see if we *can* upgrade the tech level of the mex by comparing it to myTechLevel
-					local su, sm = math.round(Spring.GetTeamRulesParam(unitTeam, "supplyUsed") or 0), math.round(Spring.GetTeamRulesParam(myTeamID(), "supplyMax") or 0)
-					local ec, es, ep, ei, ee = Spring.GetTeamResources(unitTeam, "energy")
-					local mc, ms, mp, mi, me = Spring.GetTeamResources(unitTeam, "metal")
-					for unitID in pairs(commanders) do
-						if teamTechLevel == 0 then
+				if Spring.GetTeamLuaAI(unitTeam) then
+					local teamTech = teamTechLevel[teamID] or 0
+					if teamTech < 4 then
+						local su, sm = math.round(Spring.GetTeamRulesParam(unitTeam, "supplyUsed") or 0), math.round(Spring.GetTeamRulesParam(unitTeam, "supplyMax") or 0)
+						local ec, es, ep, ei, ee = Spring.GetTeamResources(unitTeam, "energy")
+						local mc, ms, mp, mi, me = Spring.GetTeamResources(unitTeam, "metal")
+						local unitDefID = Spring.GetUnitDefID(unitID)
+						local commTech = commTechLevel[unitDefID] or 0
+
+						if commTech == 0 then
 							if mi >= 10 and ei >= 170 then
 								Spring.GiveOrderToUnit(unitID, CMD_MORPH_QUEUE, {}, {"shift"})
 							end
 						end
 
-						if teamTechLevel == 1 then
+						if commTech == 1 then
 							if mi >= 20 and ei >= 560 then
 								Spring.GiveOrderToUnit(unitID, CMD_MORPH_QUEUE, {}, {"shift"})
 							end
 						end
 
-						if teamTechLevel == 2 then
+						if commTech == 2 then
 							if mi >= 40 and ei >= 1040 then
 								Spring.GiveOrderToUnit(unitID, CMD_MORPH_QUEUE, {}, {"shift"})
 							end
 						end
 
-						if teamTechLevel == 3 then
+						if commTech == 3 then
 							if mi >= 80 and ei >= 3120 then
 								Spring.GiveOrderToUnit(unitID, CMD_MORPH_QUEUE, {}, {"shift"})
 							end
