8d7de863102cc014cb8d6272725475921d2eecdc
ForbodingAngel <forbodingangel@gmail.com>
2025-04-19 16:10:15 -0700

    Increase ereload time of comm weapons because GOdde does not know how to be a gentleman ;p

--- modinfo.lua
+++ modinfo.lua
@@ -13,7 +13,7 @@ return {
 	name='SplinterFaction',
 	description='SplinterFaction',
 	shortname='SF',
-	version='0.1.23', -- 0.1.23
+	version='$VERSION', -- 0.1.23
 	mutator='Official',
 	game='SplinterFaction',
 	shortGame='SF',
