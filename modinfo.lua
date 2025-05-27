7b7d792b10b092d964963decacaa7be1ef57fd48
ForbodingAngel <forbodingangel@gmail.com>
2025-05-26 23:25:46 -0700

    Remove the extra 5 range from fedcomm

--- modinfo.lua
+++ modinfo.lua
@@ -13,7 +13,7 @@ return {
 	name='SplinterFaction',
 	description='SplinterFaction',
 	shortname='SF',
-	version='0.1.33', -- 0.1.33
+	version='$VERSION', -- 0.1.33
 	-- DON'T FORGET TO UPDATE THE AUTOHOST!
 	mutator='Official',
 	game='SplinterFaction',
