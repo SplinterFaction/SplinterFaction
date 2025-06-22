0562e5c657f15c77bfab868c940bde2a7d07cd2b
ForbodingAngel <forbodingangel@gmail.com>
2025-06-22 09:20:04 -0700

    Better mex description

--- modinfo.lua
+++ modinfo.lua
@@ -13,7 +13,7 @@ return {
 	name='SplinterFaction',
 	description='SplinterFaction',
 	shortname='SF',
-	version='0.1.41', -- 0.1.41
+	version='$VERSION', -- 0.1.41
 	-- DON'T FORGET TO UPDATE THE AUTOHOST!
 	mutator='Official',
 	game='SplinterFaction',
