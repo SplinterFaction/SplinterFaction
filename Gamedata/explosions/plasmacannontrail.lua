c17f80879c83796183f94570dd59b13e08659cf3
ForbodingAngel <forbodingangel@gmail.com>
2025-08-31 02:13:42 -0700

    Add projectile and trail so that I can give Loz ballistic weapons while still remaining distinct from fed

--- Gamedata/explosions/plasmacannontrail.lua
+++ Gamedata/explosions/plasmacannontrail.lua
@@ -22,6 +22,29 @@ return {
 
 		},
 	},
+	["plasmacannontrail-red"] = {
+		flame = {
+			air                = true,
+			class              = [[CBitmapMuzzleFlame]],
+			count              = 1,
+			ground             = true,
+			underwater         = true,
+			water              = true,
+			properties = {
+				colormap           = [[0.9 0.2 0.2 0.006   0.9 0.2 0.2 0.005   0.9 0.1 0.1 0.004   0 0 0 0.01]],
+				dir                = [[dir]],
+				frontoffset        = 0,
+				fronttexture       = [[none]],
+				length             = -3.2,
+				sidetexture        = [[muzzleside]],
+				size               = 2.4,
+				sizegrowth         = -0.05,
+				ttl                = 3,
+				useairlos          = true,
+			},
+
+		},
+	},
 
 }
 
