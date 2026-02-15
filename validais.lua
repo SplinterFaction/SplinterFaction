b0d1a281f9535e52f9987284e911e6371fb8158d
ForbodingAngel <forbodingangel@gmail.com>
2026-02-14 21:48:44 -0800

    Add the rest of the new buildpics

---
+++ validais.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    name = 'Simple*AI',
+    desc = 'AI for SplinterFaction'
+  },
+  {
+    name = 'ChickensAI',
+	desc = 'Chicken Defence. Spawns waves of enemies.'
+  }
+  {
+    name = 'Sandbox',
+	desc = 'Does absolutely Nothing',
+  }
+}
