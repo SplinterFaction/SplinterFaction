2eb0568f0dbb11eb1e4f159e85e9552ec7eae3f9
ForbodingAngel <forbodingangel@gmail.com>
2026-02-14 21:51:35 -0800

    Switching back to

--- modinfo.lua
+++ modinfo.lua
@@ -13,7 +13,7 @@ return {
 	name='SplinterFaction',
 	description='SplinterFaction',
 	shortname='SF',
-	version='0.1.5', -- 0.1.5
+	version='$VERSION', -- 0.1.5
 	-- DON'T FORGET TO UPDATE THE AUTOHOST!
 	mutator='Official',
 	game='SplinterFaction',
