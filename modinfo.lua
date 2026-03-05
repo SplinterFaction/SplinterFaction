60a71f43aec3cf33e8e39dc4031896f20b2c043e
ForbodingAngel <forbodingangel@gmail.com>
2026-03-04 17:57:17 -0800

    0.1.63

--- modinfo.lua
+++ modinfo.lua
@@ -13,7 +13,7 @@ return {
 	name='SplinterFaction',
 	description='SplinterFaction',
 	shortname='SF',
-	version='$VERSION', -- 0.1.62
+	version='0.1.63', -- 0.1.63
 	-- DON'T FORGET TO UPDATE THE AUTOHOST!
 	mutator='Official',
 	game='SplinterFaction',
