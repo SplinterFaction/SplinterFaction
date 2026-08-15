567da9d5fe8408e3f808df8923bb2559d1a8248f
ForbodingAngel <forbodingangel@gmail.com>
2026-08-15 12:49:37 -0700

    0.1.82 0.1.81 did not have TomJNs cursor fixes pr

--- modinfo.lua
+++ modinfo.lua
@@ -13,7 +13,7 @@ return {
 	name='SplinterFaction',
 	description='SplinterFaction',
 	shortname='SF',
-	version='0.1.81', -- 0.1.81
+	version='0.1.82', -- 0.1.82
 	-- DON'T FORGET TO UPDATE THE AUTOHOST!
 	mutator='Official',
 	game='SplinterFaction',
